701ce95fe8d97a3efa282790a8c5c2e04dfa1e41
ori92 <orieliyahu92@gmail.com>
2021-08-14 05:11:38 +0300

    Added sleep in the timer function, less CPU usage

--- functions.zsh
+++ functions.zsh
@@ -25,6 +25,7 @@ stimer() (
   date1=$(($(date +%s) + $seconds))
   while [ "$date1" -ge $(date +%s) ]; do
     echo -ne "$(date -u --date @$(($date1 - $(date +%s))) +%H:%M:%S)\r"
+    sleep .5
   done
   kdialog --msgbox "$2"
 )

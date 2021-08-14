b21b0240bbec6b3ec1d204db9f10e5fb5ee0d943
ori92 <orieliyahu92@gmail.com>
2021-08-14 04:59:55 +0300

    exa instead of lsd, minor changes.

--- aliases.zsh
+++ aliases.zsh
@@ -3,25 +3,20 @@
 # Modified aliases from common-aliases plugin.
 # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/common-aliases
 
-# ls, the common ones I use a lot shortened for rapid fire usage
-alias l='lsd -A'                                                    #size,show type,human readable
-alias la='lsd -lAFh'                                                #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'                                                 #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'                                                 #long list,sorted by date,show type,human readable
-alias ll='lsd -lA'                                                  #long list
-alias ldot='lsd -ld .*'
-alias lS='lsd --total-size -l --sort=size'
-#alias lart='ls -1Fcart'
-#alias lrt='ls -1Fcrt'
+# ls - the common ones I use a lot, shortened for rapid fire usage
+alias l='exa -a --icons --group-directories-first'                                      #size,show type,human readable
+alias lr='exa -laR --icons --links --sort=date --color-scale --group-directories-first' #sorted by date,recursive,show type,human readable
+alias lt='exa -la --icons --links --sort=date --color-scale --group-directories-first'  #long list,sorted by date,show type,human readable
+alias ll='exa -la --icons --links --group-directories-first'                            #long list
+alias ldot='exa -la --icons .* --links --group-directories-first'                       #show only dot files
+alias lS='lsd --total-size -l --sort=size'                                              #show folder total size
 
-alias zshrc='${=EDITOR} ${ZDOTDIR:-$HOME}/.zshrc'                   # Quick access to the .zshrc file
-
+alias zshrc="${EDITOR} ${ZDOTDIR:-$HOME}/.zshrc" # Quick access to the .zshrc file
 # Command line head / tail shortcuts
 alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| grep'
 alias -g L="| less"
-alias -g M="| most"
 alias -g LL="2>&1 | less"
 alias -g CA="2>&1 | cat -A"
 alias -g NE="2> /dev/null"
@@ -29,31 +24,24 @@ alias -g NUL="> /dev/null 2>&1"
 alias -g P="2>&1| pygmentize -l pytb"
 
 alias du='dust -r'
-#alias dud='du -d 1 -h'
-#alias duf='du -sh *'
-#alias ff='find . -type f -name'
-
-alias h='history'
-alias hgrep="fc -El 0 | grep"
-#alias help='man'
 alias p='procs --sortd=cpu --watch-interval=1'
 alias sortnr='sort -n -r'
-#alias unexport='unset'
 
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
+alias rm='rm -iv'
+alias cp='cp -iv'
+alias mv='mv -v'
 
-#mv and cp using rsyng with progress
+# mv and cp using rsyng with progress
 alias rcp='rsync -aP'
 alias rmv='rsync -aP --remove-source-files'
 
-alias vi='nvim'
-alias vim='nvim'
-alias v='nvim'
-alias edit='nvim'
+# vim is now a function (functions.zsh)
+alias vi='v'
+alias vim='v'
+alias edit='v'
 alias gedit='subl'
 
+# Package manager
 alias updt='pamac upgrade'
 alias u='updt'
 alias rem='sudo pacman -R'
@@ -64,17 +52,17 @@ alias t='tail -f'
 alias w='watch -n 0.5 '
 alias cf='copyfile'
 alias st='sudo touch'
-alias gc='git clone'
 alias e='echo'
 alias c='cat'
 alias cc='ccat'
 alias less='cless'
 alias edot='code /home/ori/Projects/dotfiles'
+alias vdot='tmux new-window -c /home/ori/Projects/dotfiles lvim'
 alias locate='plocate'
 alias r='ranger'
 alias f='locate'
 alias sens='watch -d -n.5 sensors' #Sensors
-alias core='sudo i7z' #CPU info
+alias core='sudo i7z'              #CPU info
 
 alias mp='sudo modprobe '
 alias serv='sudo python -m http.server'
@@ -85,13 +73,16 @@ alias clip='xargs echo -n | xclip -selection clipboard'
 alias msgbox='kdialog --msgbox '
 alias grep='rg -i'
 alias m='math'
-alias srm="sudo rm -rf "
-alias q='fuck'
 alias fz='z `z| fzf`'
 alias lfp='fd -d 1 -a -H' # Show full path of files in directory.
 alias udb='sudo updatedb'
 alias send_to_phone='kdeconnect-cli -d $(kdeconnect-cli -a --id-only) --ping-msg '
 alias comp='compdef _gnu_generic ' # Generate completions for command
+
+# Git
+alias gc='git clone'
+alias gp='git pull'
+alias gs='git status'
 
 alias fu='funky'
 alias gfu='gfunky'
@@ -123,15 +114,15 @@ alias -g A,5="|awk -F, '{print \$5}'"
 
 # open browser on urls
 _browser_fts=(htm html de org net com at cx nl se dk)
-for ft in $_browser_fts; do alias -s $ft='$BROWSER'; done
+for ft in $_browser_fts; do alias -s $ft="$BROWSER"; done
 
 # open text files in editor
 _editor_fts=(js md MD cpp cxx cc c hh h inl asc txt TXT tex)
-for ft in $_editor_fts; do alias -s $ft='$EDITOR'; done
+for ft in $_editor_fts; do alias -s $ft="$EDITOR"; done
 
 # open images in viewer
 _image_fts=(jpg jpeg png gif mng tiff tif xpm)
-for ft in $_image_fts; do alias -s $ft='$XIVIEWER'; done
+for ft in $_image_fts; do alias -s $ft="$XIVIEWER"; done
 
 # open video on smplayer
 _media_fts=(ape avi flv m4a mkv mov mp3 mpeg mpg ogg ogm rm wav webm)
@@ -150,7 +141,7 @@ alias -s rar='unrar l'
 alias -s tar='tar tf'
 alias -s gz='gunzip -l'
 
-#systemd 
+#systemd
 alias s='systemctl status '
 alias us='systemctl --user status '
 alias status='sudo systemctl status'
@@ -183,22 +174,13 @@ alias R='restart '
 alias E='enable '
 alias DI='disable '
 
-#Make zsh know about hosts already accessed by SSH
-zstyle -e ':completion:*:(ssh|scp|sftp|rsh|rsync):hosts' hosts 'reply=(${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) /dev/null)"}%%[# ]*}//,/ })'
-alias -s ace='unace l'
-# disable sort when completing `git checkout`
-zstyle ':completion:*:git-checkout:*' sort false
-# set descriptions format to enable group support
-zstyle ':completion:*:descriptions' format '[%d]'
-# set list-colors to enable filename colorizing
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
-# preview directory's content with exa when completing cd
-zstyle ':fzf-tab:complete:cd:*' fzf-preview 'exa -1 --color=always $realpath'
-# switch group using `,` and `.`
-zstyle ':fzf-tab:*' switch-group ',' '.'
+alias reboot='systemctl reboot'
+alias poweroff='systemctl poweroff'
 
+alias ':q'='exit' # Force of habbit
 
 #Colors
+export no_color='\033[0m'
 export black='\033[0;30m'
 export dgray='\033[1;30m'
 export red='\033[0;31m'
@@ -215,6 +197,6 @@ export cyan='\033[0;36m'
 export lcyan='\033[1;36m'
 export lgray='\033[0;37m'
 export white='\033[1;37m'
-export no_color='\033[0m'
+
 #list of colors for easy echo
-export colors='no_color,black,dgray,red,lred,green,lgreen,orange,yellow,blue,lblue,purple,lpurple,cyan,lcyan,lgray,white'
+export colors='no_color black dgray red lred green lgreen orange yellow blue lblue purple lpurple cyan lcyan lgray white'

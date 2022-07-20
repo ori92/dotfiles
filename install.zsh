439aad5cc70fde34bfce7605815e277a39d4bc40
ori92 <orieliyahu92@gmail.com>
2022-07-20 16:23:14 +0300

    Major refactoring.

--- install.zsh
+++ install.zsh
@@ -1,86 +1,69 @@
 #!/bin/zsh
 
-# Installation script was tested on a vanilla Manjaro linux,
+# * Installation script was tested on a vanilla Manjaro linux,
 # TODO make it more readable, some more tests..
 
 [ -n "$XDG_CONFIG_HOME" ] || {
-    echo "${red}\$XDG_CONFIG_HOME is undefined, setting it to ~/.config"
+    echo "${r}XDG_CONFIG_HOME is undefined, setting it to $HOME/.config${n}"
     export XDG_CONFIG_HOME="$HOME/.config"
 }
 
-export no_color='\033[0m'
-export red='\033[0;31m'
-export green='\033[0;32m'
+# for readability
+X=$XDG_CONFIG_HOME
 
-read "answer?Are you sure? [y/N]"
-if [[ $answer =~ ^[Yy]$ ]]; then
+# colors
+n='\033[0m'    # normal
+r='\033[0;31m' # red
+g='\033[0;32m' # green
 
-    echo "${green}->installing dependencies.."
-    sudo pacman -S tmux git neovim starship xclip fzf exa zoxide lsd dust procs ranger bpytop fd plocate ripgrep neofetch
+echo "${g}-->Installing dependencies..${n}"
+sudo pacman -S tmux git neovim starship xclip fzf exa zoxide lsd dust procs ranger bpytop fd plocate ripgrep neofetch
 
-    echo "${green}->Creating backups..."
-    [ -d "$XDG_CONFIG_HOME/zsh" ] && mv $XDG_CONFIG_HOME/zsh{,.bak} && echo "->$XDG_CONFIG_HOME/zsh.bak."
-    [ -d "$HOME/.zshrc" ] && mv $HOME/.zshrc{,.bak} && echo "->$HOME/.zshrc.bak."
-    [ -d "$HOME/.zshenv" ] && mv $HOME/.zshenv{,.bak} && echo "->$HOME/.zshenv.bak."
-    [ -d "$XDG_CONFIG_HOME/zsh" ] && mv $XDG_CONFIG_HOME/zsh{,.bak} && echo "->$XDG_CONFIG_HOME/zsh.bak."
-    [ -d "$XDG_CONFIG_HOME/zsh" ] && mv $XDG_CONFIG_HOME/zsh{,.bak} && echo "->$XDG_CONFIG_HOME/zsh.bak."
-    [ -d "$XDG_CONFIG_HOME/tmux" ] && mv $XDG_CONFIG_HOME/tmux{,.bak} && echo "->$XDG_CONFIG_HOME/tmux.bak."
-    [ -d "$XDG_CONFIG_HOME/starship.toml" ] && mv $XDG_CONFIG_HOME/starship.toml{,.bak} && echo "->$XDG_CONFIG_HOME/starship.toml"
-    [ -d "$XDG_CONFIG_HOME/astronvim/" ] && mv $XDG_CONFIG_HOME/astronvim/{,.bak} && echo "->$XDG_CONFIG_HOME/astronvim.bak."
-    echo "${green}->Done."
+echo "${g}-->Creating backups...${n}"
+files=("$HOME/.zshrc" "$HOME/.zshenv" "$X/zsh" "$X/tmux" "$X/starship.toml" "$X/astronvim/" "$X/nvim")
+for f in $files; do
+    [ -d "$f" ] && mv $f{,.bak} && echo "${g}${f}-->${f}.bak${n}"
+done
+echo "${g}-->Creating directories...${n}"
+mkdir -p "$X/zsh" "$X/tmux" "$X/astronvim/lua/user"
 
-    read "answer?Install oh-my-zsh? [y/N]"
-    if [[ $answer =~ ^[Yy]$ ]]; then
-        echo "${green}Installing oh-my-zsh.."
-        echo "${red}->Please exit zsh by typing 'exit' after omz installation is finished in order to complete this installation process."
-        read "?Enter to continue.."
-        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-        mv "$HOME/.oh-my-zsh" "$XDG_CONFIG_HOME/.oh-my-zsh"
+echo "${g}Installing oh-my-zsh..${n}"
+echo "${r}-->Please exit zsh by typing 'exit' after omz installation is finished in order to complete this installation process.${n}"
+read "?ENTER to continue with installation."
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv "$HOME/.oh-my-zsh" "$X/.oh-my-zsh"
 
-        echo "${green}Installing oh-my-zsh plugins.."
-        export ZSH_CUSTOM=$XDG_CONFIG_HOME/.oh-my-zsh/custom
-        git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
-        git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/you-should-use
-        git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
-        git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
-        git clone https://github.com/hlissner/zsh-autopair $ZSH_CUSTOM/plugins/zsh-autopair
-        git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-    fi
+echo "${g}-->Installing oh-my-zsh plugins..${n}"
+export ZSH_CUSTOM=$X/.oh-my-zsh/custom
+git clone https://github.com/TamCore/autoupdate-oh-my-zsh-plugins $ZSH_CUSTOM/plugins/autoupdate
+git clone https://github.com/MichaelAquilina/zsh-you-should-use $ZSH_CUSTOM/plugins/you-should-use
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/Aloxaf/fzf-tab $ZSH_CUSTOM/plugins/fzf-tab
+git clone https://github.com/hlissner/zsh-autopair $ZSH_CUSTOM/plugins/zsh-autopair
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 
-    echo "${green}Installing zsh rc files.."
-    mkdir -p "$XDG_CONFIG_HOME/zsh"
-    ln zsh/.zlogin zsh/profile.zsh zsh/.zshrc zsh/functions.zsh zsh/pentest.zsh zsh/aliases.zsh zsh/filetype.zsh zsh/colors.zsh "$XDG_CONFIG_HOME/zsh/"
-    ln zsh/.zshenv $HOME/
+# remove the newly created .zshrc
+rm $HOME/.zshrc
 
-    echo "${green}Installing tmux rc files.."
-    mkdir -p "$XDG_CONFIG_HOME/tmux"
-    ln tmux/tmux.conf tmux/tmux.remote.conf tmux/tmux.appearance.conf "$XDG_CONFIG_HOME/tmux/"
-    ln -s $(pwd)/tmux/scripts "$XDG_CONFIG_HOME/tmux/"
-    echo "${green}Installing tmux-plugin manager.."
-    echo "${red}Remember to press prefix(C-b)+I to install plugins after you first run tmux."
-    read "?Enter to continue.."
-    git clone https://github.com/tmux-plugins/tpm "$XDG_CONFIG_HOME/tmux/plugins/tpm"
+echo "${g}-->Installing zsh rc files..${n}"
+ln zsh/.zlogin zsh/profile.zsh zsh/.zshrc zsh/functions.zsh zsh/pentest.zsh zsh/aliases.zsh zsh/filetype.zsh zsh/colors.zsh "$X/zsh/"
+ln zsh/.zshenv $HOME/
 
-    echo "${green}Installing starship rc files.."
-    ln starship/starship.toml "$XDG_CONFIG_HOME/"
+echo "${g}-->Installing tmux rc files..${n}"
+ln tmux/tmux.conf tmux/tmux.remote.conf tmux/tmux.appearance.conf "$X/tmux/"
+ln -s $(pwd)/tmux/scripts "$X/tmux/"
+echo "${g}-->Installing tmux-plugin manager..${n}"
+echo "${r}Remember to press prefix(C-b)+I to install plugins after you first run tmux.${n}"
+git clone https://github.com/tmux-plugins/tpm "$X/tmux/plugins/tpm"
 
-    read "answer?install astronvim? [y/N]"
-    if [[ $answer =~ ^[Yy]$ ]]; then
-        echo "${green}Installing oh-my-zsh.."
-        git clone https://github.com/AstroNvim/AstroNvim "$XDG_CONFIG_HOME/nvim"
-        echo "${red}Exit vim with :q after packer has finished to install the plugins.."
-        read "?Enter to continue.."
-        nvim +PackerSync
-    fi
- 
-    echo "Installing astronvim rc files.."
-    mkdir -p "$XDG_CONFIG_HOME/astronvim/lua/user"
-    git clone https://github.com/ori92/astronvim_config astronvim
-    ln -sf $(pwd)/astronvim "$XDG_CONFIG_HOME/astronvim/lua/user"
-    echo "${red}Exit vim with :q after packer has finished to install the plugins.."
-    read "?Enter to continue.."
-    nvim +PackerSync
+echo "${g}-->Installing starship rc files..${n}"
+ln starship/starship.toml "$X/"
 
-    echo "${green}Done. Have fun!!"
-fi
-echo "Exiting."
+echo "${g}-->Installing AstroNvim..${n}"
+git clone https://github.com/AstroNvim/AstroNvim "$X/nvim"
+echo "-->Installing astronvim rc files..${n}"
+git clone https://github.com/ori92/astronvim_config astronvim
+ln -sf $(pwd)/astronvim "$X/astronvim/lua/user"
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
+echo "${g}-->Done. Have fun!!${n}"

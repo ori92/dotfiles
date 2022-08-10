68dc4f824231c2c94eed4d574c20a0ece1013259
ori92 <orieliyahu92@gmail.com>
2022-08-10 16:08:49 +0300

    split configuration to multiple files

---
+++ lvim/builtin.lua
@@ -0,0 +1,18 @@
+-- Builtin Plugin Options
+
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "startify"
+lvim.builtin.notify.active = true
+lvim.builtin.terminal.active = false
+lvim.builtin.nvimtree.setup.view.side = "left"
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = true
+lvim.builtin.nvimtree.setup.filters.custom = { "^.git$", "^.vscode$" }
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.treesitter.rainbow.enable = true
+lvim.builtin.treesitter.matchup.enable = true
+lvim.builtin.treesitter.ensure_installed =
+{ "bash", "c", "javascript", "json", "lua", "python", "rust", "java", "yaml" }
+lvim.builtin.treesitter.highlight.enabled = true
+lvim.builtin.telescope.defaults.file_ignore_patterns = { ".git", "node_modules" }
+lvim.builtin.alpha.startify.section.top_buttons.entries = { { "e", "  New File", "<CMD>ene!<CR>" },
+  { "q", "  Quit", "<CMD>q!<CR>" } }
---
+++ lvim/options.lua
@@ -0,0 +1,17 @@
+-- Options
+
+lvim.colorscheme = "onedarker"
+lvim.format_on_save = true
+lvim.log.level = "warn"
+vim.opt.cmdheight = 1
+vim.opt.colorcolumn = "99999"
+vim.opt.list = true
+vim.opt.timeoutlen = 50
+vim.opt.listchars = {
+  tab = "→ ",
+  extends = "⟩",
+  precedes = "⟨",
+  trail = "·",
+  nbsp = "␣",
+  eol = "↲",
+}

b3d1775dfd04670dfb5dadea09eb00b6fe449a68
ori92 <orieliyahu92@gmail.com>
2022-08-15 00:46:12 +0300

    tokyonight colorscheme.

--- lvim/options.lua
+++ lvim/options.lua
@@ -1,8 +1,11 @@
 -- Options
 
 
-vim.g.material_style = "darker"
-lvim.colorscheme = "material"
+-- vim.g.material_style = "darker"
+
+vim.g.tokyonight_style = "night"
+lvim.colorscheme = "tokyonight"
+-- lvim.builtin.lualine
 lvim.format_on_save = false
 lvim.log.level = "warn"
 vim.opt.scrolloff = 10

c07ca90d1709126a26b117f00bcd0d25beed6244
ori92 <orieliyahu92@gmail.com>
2022-08-31 17:54:09 +0300

    update: theme, E for edit, cleanup.

--- ranger/scope.sh
+++ ranger/scope.sh
@@ -3,18 +3,6 @@
 set -o noclobber -o noglob -o nounset -o pipefail
 IFS=$'\n'
 
-## If the option `use_preview_script` is set to `true`,
-## then this script will be called and its output will be displayed in ranger.
-## ANSI color codes are supported.
-## STDIN is disabled, so interactive scripts won't work properly
-
-## This script is considered a configuration file and must be updated manually.
-## It will be left untouched if you upgrade ranger.
-
-## Because of some automated testing we do on the script #'s for comments need
-## to be doubled up. Code that is commented out, because it's an alternative for
-## example, gets only one #.
-
 ## Meanings of exit codes:
 ## code | meaning    | action of ranger
 ## -----+------------+-------------------------------------------

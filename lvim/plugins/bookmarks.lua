2fcb057d41a056e4d1256d5afceb1704b09c6312
Ori Eliyahu <orieliyahu92@gmail.com>
2023-01-29 19:04:42 +0200

    update: removed redundant "requires".

--- lvim/plugins/bookmarks.lua
+++ lvim/plugins/bookmarks.lua
@@ -1,7 +1,6 @@
 return {
     "crusj/bookmarks.nvim",
     branch = "main",
-    requires = { "kyazdani42/nvim-web-devicons" },
     config = function()
         require("bookmarks").setup()
     end,
--- lvim/plugins/trouble.lua
+++ lvim/plugins/trouble.lua
@@ -1,6 +1,5 @@
 return { -- Trouble A pretty list for showing diagnostics, references, telescope results, quickfix and location lists
     "folke/trouble.nvim",
-    requires = "kyazdani42/nvim-web-devicons",
     config = function()
         require("trouble").setup()
     end,
